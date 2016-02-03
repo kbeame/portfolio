ad56db883e1f0b40506cd4e7e7a94b268ff933b2
Katherine Beame <katherine.beame@gmail.com>
2016-02-02 22:35:13 -0800

    have a working tab function for the nav bar and content in new about section

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,8 @@ handleNav = function() {
   $('nav').on('click', '.tab', function() {
     var navVal = $(this).data('content');
     console.log(navVal);
+    $('.close-tab').hide();
+    $('section#' + navVal).fadeIn();
   });
 
   $('nav .tab:first').click();
