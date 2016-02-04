3ad62489eaab24af5d3b938b5a5f1cc0dd993624
Katherine Beame <katherine.beame@gmail.com>
2016-02-03 21:05:31 -0800

    added diff style color (theme) created a categories section. Added readmore function

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,5 @@
+
+//nav tab feature
 handleNav = function() {
   $('nav').on('click', '.tab', function() {
     var navVal = $(this).data('content');
@@ -10,3 +12,16 @@ handleNav = function() {
 };
 
 handleNav();
+
+//allow readmore function to display more than 2 paragraphs
+
+readMorePortfolio = function() {
+  $('.project-description *:nth-of-type(n+2)').hide();
+
+  $('#to-portfolio').on('click', 'a.read-on', function(event) {
+    event.preventDefault();
+    $(this).parent().find('*').fadeIn('slow');
+    $(this).hide();
+  });
+};
+readMorePortfolio();
