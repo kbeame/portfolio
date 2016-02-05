b70fc6ac38031cdb5046456fd8a499d19e7aee2b
Katherine Beame <katherine.beame@gmail.com>
2016-02-04 16:54:56 -0800

    got handlebar template for portfolio creation working

--- js/print.js
+++ js/print.js
@@ -11,23 +11,31 @@ function PortfolioCreation (options) {
 }
 
 PortfolioCreation.prototype.toHtml = function () {
-  //create new portfolio articele
-  var $newPortfolio = $('article.template').clone();
-  $newPortfolio.removeClass('template');
+  var template = Handlebars.compile($('#a-template').text());
 
-  if (!this.datePublished) {
-    $newPortfolio.addClass('draft');
-  }
-  //append to the newPortfolio
-  $newPortfolio.find('header h2').html(this.name);
-  $newPortfolio.find('time').html(parseInt((new Date() - new Date(this.datePublished))/60/60/24/1000) + ' days ago');
-  // $newPortfolio.find('time').html(parseInt((new Date() - new Date(this.datePublished))));
-  $newPortfolio.find('a#portfolio-link').attr('href', this.githubUrl);
-  $newPortfolio.find('.project-description').html(this.description);
-  // $newPortfolio.append('<hr>');
-  return $newPortfolio;
-  $('article.draft').hide();
+  this.timeSince = parseInt((new Date() - new Date(this.datePublished))/60/60/24/1000);
+  this.publishedDatePrint = this.datePublished ? 'published: ' + this.timeSince + ' days ago' : '(draft)';
+
+  return template(this);
 };
+
+  //create new portfolio articele
+  // var $newPortfolio = $('article.template').clone();
+  // $newPortfolio.removeClass('template');
+  //
+  // if (!this.datePublished) {
+  //   $newPortfolio.addClass('draft');
+  // }
+  // //append to the newPortfolio
+  // $newPortfolio.find('header h2').html(this.name);
+  // $newPortfolio.find('time').html(parseInt((new Date() - new Date(this.datePublished))/60/60/24/1000) + ' days ago');
+  // // $newPortfolio.find('time').html(parseInt((new Date() - new Date(this.datePublished))));
+  // $newPortfolio.find('a#portfolio-link').attr('href', this.githubUrl);
+  // $newPortfolio.find('.project-description').html(this.description);
+  // // $newPortfolio.append('<hr>');
+  // return $newPortfolio;
+  // $('article.draft').hide();
+// };
 
 
 
