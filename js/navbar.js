ddb81a0501b8d42cb2fc03c5b36846990cde7957
Katherine Beame <katherine.beame@gmail.com>
2016-02-09 20:38:18 -0800

    Made all functions IFFY's appended to the window

--- js/navbar.js
+++ js/navbar.js
@@ -1,38 +1,41 @@
-// It is important to keep functions as methods within objects to keep them organized.
-var portfolioView = {};
-
-//nav tab feature
-portfolioView.handleNav = function() {
-  $('nav').on('click', '.tab', function() {
-    var navVal = $(this).data('content');
-    console.log(navVal);
+(function(module) {
+  // It is important to keep functions as methods within objects to keep them organized.
+  var portfolioView = {};
+  //nav tab feature
+  portfolioView.handleNav = function() {
+    $('nav').on('click', '.tab', function() {
+      var navVal = $(this).data('content');
+      console.log(navVal);
+      $('.close-tab').hide();
+      $('section#' + navVal).fadeIn();
+    });
     $('.close-tab').hide();
-    $('section#' + navVal).fadeIn();
+    //I don't want the 'portfolio' info to show up first. I only want it if I click on portfolio.
+    // $('nav .tab:first').click();
+  };
+
+  portfolioView.readMorePortfolio = function() {
+    $('.project-description *:nth-of-type(n+2)').hide();
+
+    $('#to-portfolio').on('click', 'a.read-on', function(event) {
+      event.preventDefault();
+      $(this).parent().find('*').fadeIn('slow');
+      $(this).hide();
+    });
+  };
+
+  portfolioView.printToIndex = function() {
+    PortfolioCreation.all.forEach(function (element) {
+      $('.individualPrint').append(element.toHtml());
+    });
+  };
+
+
+
+  $(document).ready(function() {
+    portfolioView.handleNav();
+    portfolioView.readMorePortfolio();
   });
-  $('.close-tab').hide();
-  //I don't want the 'portfolio' info to show up first. I only want it if I click on portfolio.
-  // $('nav .tab:first').click();
-};
-
-portfolioView.readMorePortfolio = function() {
-  $('.project-description *:nth-of-type(n+2)').hide();
-
-  $('#to-portfolio').on('click', 'a.read-on', function(event) {
-    event.preventDefault();
-    $(this).parent().find('*').fadeIn('slow');
-    $(this).hide();
-  });
-};
-
-portfolioView.printToIndex = function() {
-  PortfolioCreation.all.forEach(function (element) {
-    $('.individualPrint').append(element.toHtml());
-  });
-};
-
-
-
-$(document).ready(function() {
-  portfolioView.handleNav();
-  portfolioView.readMorePortfolio();
-});
+  
+  module.portfolioView = portfolioView;
+})(window);
