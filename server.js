edb2abc99b507543e53e2fdc54a748df595291ea
Katherine Beame <katherine.beame@gmail.com>
2016-02-15 22:48:04 -0800

    ignored node_modules but added content to server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express'),
+  port = process.env.PORT || 3000,
+  app = express();
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
