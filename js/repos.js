a7cd0d4fb3e8abd80ce20be7392c8c904550c7f4
Katherine Beame <katherine.beame@gmail.com>
2016-02-16 19:31:12 -0800

    added ajx function to repos.js

--- js/repos.js
+++ js/repos.js
@@ -1,1 +1,26 @@
 //repos
+(function(module) {
+  var repos = {};
+  repos.all =[];
+//explain what this function does
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/users/kbeame/repos' + '?per_page=5&sort=updated',
+      type: 'GET',
+      headers: { 'Authorization': 'token' + githubToken },
+      success: function(data, message, xhr) {
+        repos.all = data;
+      }
+    }).done(function() {
+      callback();
+    });
+  };
+//explain what this function does
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo){
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
